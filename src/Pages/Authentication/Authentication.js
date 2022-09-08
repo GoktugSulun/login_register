970f840be36d73d46741ab19ca9bed2d163b0496
GoktugSulun <goktug.sulun@hotmail.com>
2022-09-08 07:16:05 +0300

    active page is stored in localstorage so that when user refresh the page, active page is not losed

--- src/Pages/Authentication/Authentication.js
+++ src/Pages/Authentication/Authentication.js
@@ -6,10 +6,10 @@ import Login from './Components/Login';
 import AuthenticationContext from './Context/Context';
 
 const Authentication = () => {
-   const [isLoginActive, setIsLoginActive] = useState(true);
-   const [leftClassName, setLeftClassName] = useState("left left__active");
-   const [rightClassName, setRightClassName] = useState("right right__inactive");
-   const [imageClassName, setImageClassName] = useState("image image__left");
+   const [isLoginActive, setIsLoginActive] = useState(null);
+   const [leftClassName, setLeftClassName] = useState("left");
+   const [rightClassName, setRightClassName] = useState("right");
+   const [imageClassName, setImageClassName] = useState("image");
 
   const store = useMemo(() => (
       {
@@ -23,14 +23,38 @@ const Authentication = () => {
          setRightClassName("right right__active");
          setLeftClassName("left left__inactive");
          setImageClassName("image image__left");
+
+         const value = JSON.stringify({
+            isLoginActive: true,
+            rightClassName: "right right__active",
+            leftClassName: "left left__inactive",
+            imageClassName: "image image__left",
+         });
+         localStorage.setItem("authenticationClasses", value);
       }
 
       if (isLoginActive === false) {
          setRightClassName("right right__inactive");
          setLeftClassName("left left__active");
          setImageClassName("image image__right");
+
+         const value = JSON.stringify({
+            isLoginActive: false,
+            rightClassName: "right right__inactive",
+            leftClassName: "left left__active",
+            imageClassName: "image image__right",
+         });
+         localStorage.setItem("authenticationClasses", value);
       }
   }, [isLoginActive]);
+
+  useEffect(() => {
+   const authenticationClasses = JSON.parse(localStorage.getItem("authenticationClasses"));
+   setIsLoginActive(authenticationClasses.isLoginActive);
+   setRightClassName(authenticationClasses.rightClassName);
+   setLeftClassName(authenticationClasses.leftClassName);
+   setImageClassName(authenticationClasses.imageClassName);
+  }, []);
 
   return (
     <AuthenticationContext.Provider value={store}>
